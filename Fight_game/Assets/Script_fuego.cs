0a518c2522b031401880720179ce57b19eb33d9f
Raul <raul.flores.arenas@gmail.com>
2018-10-05 16:20:34 -0700

    fuego

---
+++ Fight_game/Assets/Script_fuego.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_fuego : MonoBehaviour {
+
+    public bool lanzado = false;
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb2d.transform.Translate(new Vector3(4, 0, 0) * Time.deltaTime);
+    }
+}
